da7376f1152b2ebe3729f46df238cc8c7859eff2
Stephen <stephendeanharper@gmail.com>
2018-09-25 16:21:59 -0700

    updating pagination page on project list

--- web/project_data/views.py
+++ web/project_data/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Project
 
 
 def project_list_view(request):
     # import pdb; pdb.set_trace()
-    projects = get_list_or_404(Project)
+    projects_query = get_list_or_404(Project)
+    paginator = Paginator(projects_query, 20)
+
+    page = request.GET.get('page')
+    projects = paginator.get_page(page)
 
     context = {
         'projects': projects
